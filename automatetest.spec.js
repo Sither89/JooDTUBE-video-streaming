6d9d87dabef438e11c9f290ca6d424ef023d22ef
Sither89 <thetruman89@gmail.com>
2022-10-28 00:04:04 +0700

    delete file App.test.js and merge automatetest.spec.js

--- automatetest.spec.js
+++ automatetest.spec.js
@@ -1,9 +1,42 @@
-
 describe('JoodTUBE', () => {
   beforeAll(async () => {
     await page.goto('http://localhost:9090/')
   })
 
+  it('should display login page', async () => {
+    await page.goto('http://localhost:9090/')
+    await Promise.all([  
+      page.$eval('a[id=loginpage]', element =>
+        element.click()
+      ),
+      await page.waitForNavigation(),
+      ]);
+    expect( page.url() ).toBe('http://localhost:9090/login')
+  })
+
+    
+  it('should display login page', async () => {
+    await page.goto('http://localhost:9090/')
+    await Promise.all([  
+      page.$eval('a[id=loginpage]', element =>
+        element.click()
+      ),
+      await page.waitForNavigation(),
+      ]);
+    expect( page.url() ).toBe('http://localhost:9090/login')
+  })
+
+  it('should display login page on button', async () => {
+    await page.goto('http://localhost:9090/')
+    await page.$eval( 'a[id=btnloginpage]', form => form.click() );
+    expect( page.url() ).toBe('http://localhost:9090/login')
+  })
+
+  it('should display Register page on button', async () => {
+    await page.goto('http://localhost:9090/')
+    await page.$eval( 'a[id=btnRegisterpage]', form => form.click() );
+    expect( page.url() ).toBe('http://localhost:9090/register')
+  })
   it('should display Home page', async () => {
     await Promise.all([  
       page.$eval('a#logo', element =>
@@ -96,66 +129,68 @@ describe('JoodTUBE', () => {
     expect( page.url() ).toBe('http://localhost:9090/')
   })
 
-  it('should display login page', async () => {
-    await page.goto('http://localhost:9090/')
-    await Promise.all([  
-      page.$eval('a[id=loginpage]', element =>
-        element.click()
-      ),
-      await page.waitForNavigation(),
-      ]);
-    expect( page.url() ).toBe('http://localhost:9090/login')
-  })
+  const request = require("supertest")
+const baseURL = "http://localhost:9090"
 
-  it('should display register page', async () => {
-    await page.goto('http://localhost:9090/login')
-    await Promise.all([  
-      page.$eval('a[id=registerpage]', element =>
-        element.click()
-      ),
-      await page.waitForNavigation(),
-      ]);
-    expect( page.url() ).toBe('http://localhost:9090/register')
-  })
+describe("GET /", () => {
+    it("test / page return 200", async () => {
+        const response = await request(baseURL).get("/");
+        expect(response.statusCode).toBe(200);
+    });
+});
 
-  it('should display login page on button', async () => {
-    await page.goto('http://localhost:9090/')
-    await page.$eval( 'a[id=btnloginpage]', form => form.click() );
-    expect( page.url() ).toBe('http://localhost:9090/login')
-  })
+describe("GET /login", () => {
+    it("test /login page should return 200", async () => {
+        const response = await request(baseURL).get("/login");
+        expect(response.statusCode).toBe(200);
+    });
+});
 
-  it('should display Register page on button', async () => {
-    await page.goto('http://localhost:9090/')
-    await page.$eval( 'a[id=btnRegisterpage]', form => form.click() );
-    expect( page.url() ).toBe('http://localhost:9090/register')
-  })
+describe("GET /watch_vdo", () => {
+    it("test /watch_vdo page should return 200", async () => {
+        const response = await request(baseURL).get("/watch_vdo");
+        expect(response.statusCode).toBe(200);
+    });
+});
 
- /* it('Test Login', async () => {
-   // const btn = document.querySelector('#btnlogin');
-    let selector = 'Button[id=btnfake]';
-    await page.goto('http://localhost:9090/login')
-    await page.type('input[id=username]', "phaswich.sir@dome.tu.ac.th");
-    await page.type('input[id=password]', "12345");
+describe("GET /upload_vdo", () => {
+    it("test /upload_vdo page should return 200", async () => {
+        const response = await request(baseURL).get("/upload_vdo");
+        expect(response.statusCode).toBe(200);
+    });
+});
 
-    //wait Promise.all([
-      //page.$eval('button#btnlogin.btn1.btn-primary.mx-auto.btn-lg.btn-block', element =>
-    //  page.$eval('button#btnfake', element =>
-     // element.click()
-    //  ),
-     // await page.waitForNavigation(),
-   // ]);
-    //await page.evaluate((selector) => document.querySelector(selector).click(), selector); 
-   // await Promise.all([
-    //const form = await page.$('Button[id=btnfake]');
-    //await form.evaluate( form => form.click() );
-     // await page.click('Button[id=btnfake]'),
-    //      page.waitForNavigation()
-    //  ])
-    
-    expect( page.url() ).toBe('http://localhost:9090/register')
-  
-   
+describe("GET /register", () => {
+    it("test /register page should return 200", async () => {
+        const response = await request(baseURL).get("/register");
+        expect(response.statusCode).toBe(200);
+    });
+});
 
-  })*/
+describe("GET /works-in-chrome", () => {
+    it("test /works-in-chrome page should return 200", async () => {
+        const response = await request(baseURL).get("/works-in-chrome");
+        expect(response.statusCode).toBe(200);
+    });
+});
+
+describe("POST /upload-vdo", () => {
+    it("test upload video post /upload-vdo", async () => {
+        const response = await request(baseURL).post("/upload-vdo")
+            .attach('video', './test-upload/test.mp4')
+            console.log(response.body)
+        expect(response.status).toBe(200);
+    });
+});
+
+describe("POST /upload-vdo wrong path", () => {
+    it("test upload video post /upload-vdo no path show 'NO file uploaded'", async () => {
+        const response = await request(baseURL).post("/upload-vdo")
+            .attach('videos', './test-upload/test.mp4')
+            console.log(response.body)
+        expect(response.text).toEqual("Error uploading file.");
+    });
+});
+
 
 })
