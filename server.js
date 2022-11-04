a8f3275106651f243a537794586b2ec62484cd14
Phaswich <phaswich.sir@dome.tu.ac.th>
2022-11-04 23:02:26 +0700

    Register and login.

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require("express");
 var app = express();
 var http = require('http').createServer(app);
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
+
+
+
+////////////////////////////////////////////
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+require('./db');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var bcrypt = require('bcryptjs'); 
+////////////////////////////////////////////
+
+
 
 var formidable = require('formidable');
 var fs = require('fs');
@@ -14,6 +30,7 @@ app.set("view engine", "ejs");
 
 const session = require('express-session');
 const flash = require('connect-flash');
+const { Int32 } = require("mongodb");
 
 const filePath = "videos/SampleVideo_1280x720_1mb.mp4";
 
@@ -25,9 +42,68 @@ http.listen(9090, function (req, res) {
     res.render("index");
   });
 
+
+/////////////////////////////////////
+/////////////////////////////////////
+  const contactSchema = {
+    fname: String,
+    lname: String,
+    Username: String,
+    Email: String,
+    password: String,
+    Role: String
+  };
+  
+  const Contact = mongoose.model("customers", contactSchema);
+/////////////////////////////////////
+/////////////////////////////////////
+  
+
   app.get("/login", function (req, res) {
     res.render("login");
   });
+
+  
+/////////////////////////////////////
+/////////////////////////////////////
+  app.post("/login", async function (req, res) {
+    console.log(req.body)
+    MongoClient.connect(url, async function(err, db) {
+      var val = 0;
+      const item = await Contact.find({})
+     // bcrypt.compare(req.body.password, item.password).then(function(res) {
+        // res == true/false
+     // });
+    // const saltRounds = 10;
+   // const hashp = bcrypt.hashSync(req.body.password, saltRounds);
+    // const isCorrect = bcrypt.compareSync(hashp, item.password);
+     //console.log(" " + isCorrect)
+      item.forEach(item => {
+        if(req.body.username === item.Email && val === 0){
+          if(req.body.password === item.password && val === 0){
+            if(item.Role == "Admin"){
+                res.redirect('/upload_vdo?'+ item._id)
+               val = 1 ;
+            }
+            if(item.Role == "User"){
+              res.redirect('/watch_vdo?'+ item._id)
+              val = 1 ;
+            }
+              console.log("pass");
+          }else{
+            val = 0 ;
+          }
+      }else{
+             val = 0 ;
+        }
+      });
+      if(val === 0){
+        res.render("login");
+      }
+    });
+  });
+  /////////////////////////////////////
+  /////////////////////////////////////
 
   app.get("/watch_vdo", function (req, res) {
     res.render("Watch_vdo");
@@ -41,6 +117,32 @@ http.listen(9090, function (req, res) {
     res.render("Register");
   });
 
+  /////////////////////////////////////
+  /////////////////////////////////////
+  app.post("/register", async function (req, res) {
+    //console.log(req.body)
+   // var hashp = req.body.password.toString('base64')
+   const saltRounds = 10;
+    // const hashp = bcrypt.hashSync(req.body.password, saltRounds);
+    const contact = new Contact({
+      fname: req.body.fname,
+      lname: req.body.lname,
+      Username: req.body.Username,
+      Email: req.body.Email,
+      password: req.body.password,
+      Role: "User"
+  });
+  contact.save(function (err) {
+      if (err) {
+          throw err;
+      }else{
+        res.render("login");
+      } 
+  });
+  });
+  /////////////////////////////////////
+  /////////////////////////////////////
+ 
 
   app.get("/works-in-chrome", (req, res) => {
     res.setHeader("content-type", "video/mp4");
@@ -96,3 +198,4 @@ http.listen(9090, function (req, res) {
   });
 
 });
+
