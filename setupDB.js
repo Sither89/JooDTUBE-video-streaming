b8dc46c588c380ffab7ce6b50da3f44d8a077789
Phaswich <phaswich.sir@dome.tu.ac.th>
2022-11-18 14:41:40 +0700

    Update setupDB.

--- setupDB.js
+++ setupDB.js
@@ -1,12 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var dbo = db.db("JoodTubeDB");
-  dbo.createCollection("customers", function(err, res) {
+  dbo.createCollection("customers", function (err, res) {
     if (err) throw err;
-   // console.log("Collection created!");
+    // console.log("Collection created!");
     db.close();
   });
 
@@ -17,8 +17,8 @@ MongoClient.connect(url, function(err, db) {
     Email: "admin",
     password: "$2a$10$93obybBMKt9XWaiXZ7GmJeXpcuyeVgWcLK7CRoUEZxCXGwRYlu70O",
     Role: "Admin"
-    };
-    
+  };
+
   const User = {
     fname: "User",
     lname: "User",
@@ -26,28 +26,42 @@ MongoClient.connect(url, function(err, db) {
     Email: "user",
     password: "$2a$10$Yv2T0rLxvbWxHSDPLHBWduIvQlEte8lhgvFInKP9W8TtcpOPhHl9O",
     Role: "User"
-    };
+  };
 
   const course = {
-      id_course: "String",
-      name: "AWS",
-      description: "Guide",
-      Teacher: "9Arm",
-    };  
+    id_course: "String",
+    name: "AWS",
+    description: "Guide",
+    Teacher: "9Arm",
+  };
 
-  dbo.collection("customers").insertOne(Admin, function(err, res) {
+  const vdo = {
+    UpFile: "MP4",
+    Title_your_assets: "CS360 Clip #1",
+    Description: "About AWS seesion",
+    Tags: "AWS",
+    Course: "Deployment for AWS"
+  };
+
+
+  dbo.collection("customers").insertOne(Admin, function (err, res) {
     if (err) throw err;
-   // console.log("1 document inserted");
+    // console.log("1 document inserted");
     db.close();
   });
-  dbo.collection("customers").insertOne(User, function(err, res) {
+  dbo.collection("customers").insertOne(User, function (err, res) {
     if (err) throw err;
-  // console.log("1 document inserted");
+    // console.log("1 document inserted");
     db.close();
   });
-  dbo.collection("course").insertOne(course, function(err, res) {
+  dbo.collection("course").insertOne(course, function (err, res) {
     if (err) throw err;
-   // console.log("1 document inserted");
+    // console.log("1 document inserted");
+    db.close();
+  });
+  dbo.collection("upload").insertOne(vdo, function (err, res) {
+    if (err) throw err;
+    // console.log("1 document inserted");
     db.close();
   });
 });
